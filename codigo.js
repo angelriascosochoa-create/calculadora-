db8947fa3499a92e84cd05103cf36abade21417b
angelriascosochoa <angelriascosochoa@gmail.com>
2026-09-21 08:12:10 -0600

    xadqdqd

---
+++ codigo.js
@@ -0,0 +1,13 @@
+let pantalla = document.getElementById("Pantalla");
+
+function agregar(dato){
+    pantalla.value += dato
+}
+
+function Eliminar(){
+    pantalla.value = "";
+}
+
+function calcular(){
+    pantalla.value
+}
